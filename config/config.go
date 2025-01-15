206ceabf7e7f76735da2aaf3dc7c08156cb20b48
Lars <lars@roth-kl.de>
2025-01-15 07:58:49 +0100

    feat(config): :sparkles: add yaml config

--- config/config.go
+++ config/config.go
@@ -1,8 +1,18 @@
 package config
 
+import (
+	"time"
+)
+
 type Config struct {
 	ListenAddress  string `env:"LISTEN_ADDR" envDefault:":8080"`
 	DeviceAddress  string `env:"DEVICE_ADDR"`
 	LogLevel       string `env:"LOG_LEVEL" envDefault:"info"`
 	UpdateInterval int64  `env:"INTERVAL" envDefault:"30"`
+}
+
+type YamlConfig struct {
+	ListenAddress string `yaml:"listenAddress"`
+	LogLevel string `yaml:"logLevel"`
+	DeviceUpdateInterval time.Duration `yaml:"deviceUpdateInterval"`
 }
